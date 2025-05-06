85a8b805ebc2e9dde2ac37f99cdbe3b367219a5d
Akash701 <akashjnair701@gmail.com>
2025-05-05 21:39:51 -0600

    Completed VPC setup

--- main.tf
+++ main.tf
@@ -1,7 +1,86 @@
 resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support = true
 
-  tags = "MainVPC"
+  tags = {
+    Name = "MainVPC"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main_vpc.id
+  
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = var.cidr_block
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1"
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = "10.0.2.0/24"    // Create a seperarte variable for this one
+  availability_zone = "us-east-1"
+  
+  tags = {
+    Name = "private_subnet"
+  }
+}
+
+resource "aws_eip" "elastic_nat" {
+  domain = "vpc"  
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  subnet_id = aws_subnet.public.id
+
+  tags={
+    Name = "main-nat"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route{
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags={
+    Name = "public-rt"
+  }
+
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  
+tags = {
+  Name = "private-rt"
+}
+
+}
+
+resource "aws_route_table_association" "public" {
+route_table_id = aws_subnet.public.id
+subnet_id = aws_subnet.public.id
+
+}
+
+resource "aws_route_table_association" "private" {
+route_table_id = aws_subnet.private.id
+subnet_id = aws_subnet.private.id
+
 }
