d41945a503d23c27c43bd4b8144704b5c0238f63
Akash <akashjnair701@gmail.com>
2026-04-12 18:34:53 -0600

    test: add costly terraform for nimbiq webhook test

---
+++ nimbiq_test.tf
@@ -0,0 +1,21 @@
+resource "aws_lambda_function" "api" {
+  function_name = "my-api"
+  timeout       = 900
+  memory_size   = 3008
+  runtime       = "python3.12"
+  handler       = "handler.main"
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = "eip-123"
+  subnet_id     = "subnet-123"
+}
+
+resource "aws_cloudwatch_log_group" "api_logs" {
+  name = "/aws/lambda/my-api"
+}
+
+resource "aws_instance" "app" {
+  ami           = "ami-123"
+  instance_type = "t2.2xlarge"
+}
